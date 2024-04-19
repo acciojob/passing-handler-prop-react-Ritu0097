f8081a7239386ab8c6d5bf0a29653c6adba980af
Ritu0097 <rituchaudhary0097@gmail.com>
2024-04-19 18:37:44 +0530

    selector

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react'
 import './child.css'
 
 const Selection = ({ applyColor }) => {
-	const bg = useState({ background: '' })[0]
+  const bg = useState({ background: '' })[0]
 
-	return (
-		<div className="fix-box" style={bg} onClick={applyColor}>
-			<h2 className="subheading">Selection</h2>
-		</div>
-	)
+  return (
+    <div
+      className="fix-box"
+      style={bg}
+      onClick={applyColor}
+      data-testid={`${bg.background}`}
+    >
+      <h2 className="subheading">Selection</h2>
+    </div>
+  )
 }
 
 export default Selection
